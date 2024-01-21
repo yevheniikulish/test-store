90340a0dfd8f09fcf537002ac89ace0b0c6c05fa
Yevhenii <s006644@vnmu.edu.ua>
2024-01-21 22:13:39 +0200

    fix email settings

--- store/settings.py
+++ store/settings.py
@@ -224,6 +224,7 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = env('EMAIL_BACKEND')
+    EMAIL_HOST = env('EMAIL_HOST')
     EMAIL_PORT = env('EMAIL_PORT')
     EMAIL_HOST_USER = env('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
